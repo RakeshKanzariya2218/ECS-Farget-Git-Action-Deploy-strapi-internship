e8a4a99cf8b6ebe3521018564d37fa3f466fab5f
Rakeshkanzariya2218 <rkanzariya1234@gmail.com>
2025-10-01 07:06:58 +0530

    final-changes

--- ECS-Terraform-infra-task-8/backend.tf
+++ ECS-Terraform-infra-task-8/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
   backend "s3" {
     bucket = "rakesh.s3bucketpearlthoughts"
-    key    = "${var.project_name}-task-8-cloudwatch-terraform.tfstate"
+    key    = "rakesh-task-8-cloudwatch-ecs-terraform.tfstate"
     region = "ap-south-1"
     encrypt      = true  
   }
